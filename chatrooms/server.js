4cc9db138d70ae18f898fbc6f1fffabcbb515c01
erizhang <zhang.lyuan@gmail.com>
2015-01-08 10:57:50 +0800

    replace splace with tab

--- chatrooms/server.js
+++ chatrooms/server.js
@@ -5,57 +5,57 @@ var mime	= require('mime');
 var cache	= {};
 
 function send404(response) {
-    response.writeHead(404, {'Content-Type': 'text/plain'});
-    response.write('Error 404: resource not found.');
-    response.end();
+	response.writeHead(404, {'Content-Type': 'text/plain'});
+	response.write('Error 404: resource not found.');
+	response.end();
 }
 
 
 function sendFile(response, filePath, fileContents) {
-    response.writeHead(200, {'Content-Type': mime.lookup(path.basename(filePath))});
-    response.end(fileContents);
+	response.writeHead(200, {'Content-Type': mime.lookup(path.basename(filePath))});
+	response.end(fileContents);
 }
 
 function serveStatic(response, cache, absPath) {
-    if (cache[absPath]) {
+	if (cache[absPath]) {
 	sendFile(response, absPath, cache[absPath]);
-    }
-    else {
-	fs.exists(absPath, function(exists){
-	    if (exists){
-		fs.readFile(absPath, function(err, data){
-		    if (err) {
-			send404(response);
-		    }
-		    else {
-			cache[absPath] = data;
-			sendFile(response, absPath, data);
-		    }
+	}
+	else {
+		fs.exists(absPath, function(exists){
+			if (exists){
+				fs.readFile(absPath, function(err, data){
+					if (err) {
+						send404(response);
+					}
+					else {
+						cache[absPath] = data;
+						sendFile(response, absPath, data);
+					}
+				});
+			}
+			else {
+				send404(response);
+			}
 		});
-	    }
-	    else {
-		send404(response);
-	    }
-	});
-    }
+	}
 }
 
 var server = http.createServer(function(request, response){
-    var filePath = false;
+	var filePath = false;
 
-    if (request.url == '/') {
-	filePath = 'public/index.html';
-    }
-    else {
-	filePath = 'public' + request.url;
-    }
+	if (request.url == '/') {
+		filePath = 'public/index.html';
+	}
+	else {
+		filePath = 'public' + request.url;
+	}
 
-    var absPath = './' + filePath;
-    serveStatic(response, cache, absPath);
+	var absPath = './' + filePath;
+	serveStatic(response, cache, absPath);
 });
 
 server.listen(8889, function(){
-    console.log("Server listening on port 8889.");
+	console.log("Server listening on port 8889.");
 });
 
 var chatServer = require('./lib/chat-server.js');
